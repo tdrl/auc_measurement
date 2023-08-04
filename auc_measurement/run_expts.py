e059090c354a352f92ee17b2b7c3680cc0a747af
Heather Lane <hlane@athenahealth.com>
2023-08-04 16:40:07 -0400

    Some cleanup and checkpoint support.

--- auc_measurement/run_expts.py
+++ auc_measurement/run_expts.py
@@ -42,10 +42,21 @@ def load_config(config_fname: Union[str, Path]) -> Config:
         return Config.schema().loads(raw.read())  # type: ignore
 
 
+def mark_complete():
+    Path('.complete').touch()
+
+
+def is_complete() -> bool:
+    return Path('.complete').exists()
+
+
 def run_one_model(X, y, cv_splits, model):
     for idx, (train, test) in enumerate(cv_splits):
         logging.info(f'    Doing split {idx}')
         with dir_stack_push(Path.cwd() / f'fold_{idx}', force_create=True):
+            if is_complete():
+                logging.info(f'    split {idx} already done; skipping.')
+                continue
             logging.info(f'    Fitting model {model}...')
             model.fit(X[train], y[train])
             logging.info('    Done.')
@@ -54,10 +65,12 @@ def run_one_model(X, y, cv_splits, model):
             logging.info('    Predicting on test fold...')
             dump(model.predict_proba(X[test]), 'predictions.joblib')
             logging.info('    Done.')
-            Path('.complete').touch()
+            mark_complete()
 
 
 def run_single_expt(config: Config, dataset: Bunch):
+    if is_complete():
+        return
     X = dataset['data']
     y = dataset['target']
     rows = X.shape[0]
@@ -72,17 +85,24 @@ def run_single_expt(config: Config, dataset: Bunch):
     for model_name, params in config.models_to_test.items():
         with dir_stack_push(Path.cwd() / model_name, force_create=True):
             logging.info(f'  Doing model {model_name}...')
+            if is_complete():
+                logging.info(f'  Model {model_name} already done; skipping.')
+                continue
             model = MODEL_REGISTRY[model_name](**params, random_state=config.random_seed)
             with open('model_params.json', 'w') as params_out:
                 json.dump(model.get_params(), params_out, indent=2)
             run_one_model(X=X, y=y, cv_splits=cv_splits, model=model)
-    Path('.complete').touch()
+            mark_complete()
+    mark_complete()
 
 
 def run_all_expts(config: Config):
     for dataset_name in config.datasets:
         logging.info(f'===== Doing {dataset_name} =====')
         with dir_stack_push(Path.cwd() / dataset_name, force_create=True) as expt_dir:
+            if is_complete():
+                logging.info(f'Dataset {dataset_name} already done; skipping.')
+                continue
             logging.info(f'Created experiment directory for dataset {dataset_name} at {expt_dir}')
             dataset = DATA_LOADER_REGISTRY[dataset_name]()
             with open('dataset_descr.txt', 'w') as descr_out:
@@ -90,6 +110,7 @@ def run_all_expts(config: Config):
             with open('dataset_name.txt', 'w') as name_out:
                 name_out.write(dataset_name + '\n')
             run_single_expt(config=config, dataset=dataset)
+            mark_complete()
 
 
 def main(config=None):
@@ -109,9 +130,13 @@ def main(config=None):
     logging.info('Creating experiment output dir at %s',
                  config.experiments_output_dir)
     with dir_stack_push(expt_dir, force_create=True) as _:
+        if is_complete():
+            logging.info('All experiments already marked done. Stopping immediately.')
+            return
         with open('config.json', 'w') as config_out:
             config_out.write(config.to_json(indent=2))  # type: ignore
         run_all_expts(config=config)
+        mark_complete()
 
 
 if __name__ == '__main__':
